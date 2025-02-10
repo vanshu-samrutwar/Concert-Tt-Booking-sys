4edf5464f2556ceac7cfb11a524d310e2a0ea6fa
vanshu-samrutwar <vanshu24june@gmail.com>
2025-02-10 21:41:20 +0530

    init commit

---
+++ src/test/java/bookingsys/concertttbookingsys/ConcertTtBookingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package bookingsys.concertttbookingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTtBookingSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
